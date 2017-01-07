98ed0170e9b8663d0c596a1d70f9caf9b375b1e7
Chicj <chic.jordan@yahoo.com>
2017-01-06 15:21:47 -0900

    COMPILING LIBS FOR START OF ARC2 SOFTWARE!

---
+++ version_old.c
@@ -0,0 +1,10 @@
+#include "ARCbus.h"
+
+#warning version not found "Error encountered while running git describe : FileNotFoundError: [WinError 2] The system cannot find the file specified"
+
+const char ARClib_version[]="unversioned";
+
+const unsigned short ARClib_major=BUS_INVALID_MAJOR_VER,ARClib_minor=BUS_INVALID_MINOR_VER;
+const unsigned short ARClib_commits=0;
+const char ARClib_hash[]="invalid";
+const unsigned short ARClib_dty=BUS_VER_DIRTY;
